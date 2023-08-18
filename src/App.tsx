a81e82969ddc482a081fceb5a47885d9733349cd
Akshat Kumar <codespicer@pop-os.localdomain>
2023-08-18 17:37:03 +0530

    adding todos functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
-  const todos
+  
+  const [todos , setTodos ] = useState<{id:string , text:string}[]>([]);
+
+  const todoHandler = ( text:string) =>{
+    setTodos([...todos ,{id: Math.random().toString() , text : text}])
+  }
+
   return (
-    <div className='App'></div>
+    <div className='App'>
+      <NewTodo onAddTodo={todoHandler}/>
+      <TodoList items={todos}/>
+    </div>
   )
 }
 
